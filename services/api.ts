8e23555bcced55963096826d667769e0cc8239f7
Ahmedadel1414 <ahmedadel10888@gmail.com>
2026-04-24 15:57:47 +0300

    create a movie details page

--- services/api.ts
+++ services/api.ts
@@ -23,7 +23,28 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   return data.results;
 };
 
-// const url =
+export const fetchMovieDetails = async (
+  movieId: string,
+): Promise<MovieDetails> => {
+  try {
+    const res = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      },
+    );
+    if (!res.ok) {
+      throw new Error(`Failed to fetch movie details: ${res.statusText}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 //   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
 // const options = {
 //   method: "GET",
